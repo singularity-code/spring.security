61a5c1ec1086cb6633521d4235ea886109a3662a
Chris <neoxrock@gmail.com>
2021-09-24 11:12:25 +1000

    Spring Security HTTP header test

--- src/main/java/com/demo/security/demosecurity/WebSecurityConfig.java
+++ src/main/java/com/demo/security/demosecurity/WebSecurityConfig.java
@@ -8,16 +8,24 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter.ReferrerPolicy;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	//secret123
+	// secret123
 	private static final String ENCODED_PASSWORD = "$2a$10$AIUufK8g6EFhBcumRRV2L.AQNz3Bjp7oDQVFiO5JJMBFZQ6x2/R/2";
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/", "/home").permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout().permitAll();
+		http.headers().frameOptions().sameOrigin().httpStrictTransportSecurity().disable();
+		http.headers().defaultsDisabled().cacheControl();
+		http.headers().defaultsDisabled().contentTypeOptions();
+		http.headers().httpStrictTransportSecurity().includeSubDomains(true).maxAgeInSeconds(31536000);
+		http.headers().frameOptions().sameOrigin();
+		http.headers().xssProtection().block(false);
+		http.headers().referrerPolicy(ReferrerPolicy.SAME_ORIGIN);
 	}
 
 	@Override
